4901a0c29e5f36c2936a388706525a0e55d8e6e2
Oleksii Samutin <samutin63@gmail.com>
2022-04-22 18:47:25 +0300

    Made all tasks

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  for (const key in obj) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+  return undefined;
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  const res = fn(...args);
+  const inputTypes = types.slice(0, types.length - 1);
+  const typeResult = types[types.length - 1];
+  if (res !== typeResult(res)) {
+    throw new Error('Types is not correct!');
+  }
+  for (let i = 0; i < inputTypes.length; i++) {
+    if (args[i] !== inputTypes[i](args[i])) {
+      throw new Error('Types is not correct!');
+    }
+  }
+  return res;
+};
+
 
 module.exports = { contract };
